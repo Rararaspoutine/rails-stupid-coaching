61b4f847be4c557f9c3157329145510a5a7979bd
Damien Gagnepain <dg.raspoutine@gmail.com>
2020-06-01 15:05:21 +0200

    ex

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @anwser = params[:answer]
-    if answer.include?(?)
-      puts 'Silly question, get dressed and go to work!'
-    elsif answer == 'I am going to work'
-      puts 'Great!'
-    else puts "I don't care, get dressed and go to work!"
+    if @anwser.include?("?")
+      @message = 'Silly question, get dressed and go to work!'
+    elsif @answer = 'I am going to work'
+      @message = 'Great!'
+    else
+      @message = "I don't care, get dressed and go to work!"
     end
   end
 end
-
-#   If the message is I am going to work, the coach will answer Great!
-# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
-# Otherwise the coach will answer I don't care, get dressed and go to work!
